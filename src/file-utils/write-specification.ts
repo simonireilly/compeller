6b558bae69cc010e1a5c72d4056a5bdf348eece9
simon <simonireilly@gmail.com>
2022-01-27 08:17:57 +0000

    Guard against production side effects in compeller

--- src/file-utils/write-specification.ts
+++ src/file-utils/write-specification.ts
@@ -2,17 +2,27 @@ import { writeFileSync } from 'fs';
 import { join } from 'path';
 import { ICompellerOpenAPIObject, ICompellerOptions } from '../compeller';
 
+/**
+ * Write a JSON object to file if a valid path if provided
+ *
+ * @default `${__dirname}/openapi.json`
+ *
+ * @param jsonSpecFile If true a default path will be used as provided, other provide a fully qualified path
+ * @param spec The OpenAPI specification object
+ */
 export const writeSpecification = (
   jsonSpecFile: ICompellerOptions['jsonSpecFile'],
   spec: ICompellerOpenAPIObject
 ) => {
   let fileName;
 
+  if (['production', 'prod'].includes(process?.env?.NODE_ENV || '')) return;
+
   try {
     fileName =
       typeof jsonSpecFile === 'string'
         ? jsonSpecFile
-        : join(process.cwd(), 'openapi.json');
+        : join(__dirname, 'openapi.json');
 
     writeFileSync(fileName, JSON.stringify(spec, undefined, 2));
   } catch (err) {
